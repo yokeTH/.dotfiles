af216e7427510c78283f8645a814d2bebe9ae7e2
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-09-20 16:02:03 +0700

    fix: version and hash

--- lib/imageoptim.nix
+++ lib/imageoptim.nix
@@ -5,11 +5,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   name = "imageoptim";
-  version = "1.5.18";
+  version = "1.9.3";
 
   src = fetchzip {
     url = "https://imageoptim.com/ImageOptim${finalAttrs.version}.tar.xz";
-    hash = "sha256-TFOdNbZG+jjrdKRMu+0mn/UJPcUMRU6/5JDjqCzVXzs=";
+    hash = "sha256-DTgGypH+lyc4T+WqSijEute5meNeZ8l2O1BPYm1rL+M=";
     stripRoot = false;
   };
 
